3ac0f59053050c350607250a72dc1b448fe8d4d4
Satamaka <55y2kkfq9t@privaterelay.appleid.com>
2025-12-29 17:59:11 +0900

    cat: Even if an error occurs midway, I stopped exiting the program with EXIT_SUCCESS and made it properly exit with EXIT_FAILURE instead. I also replaced the magic numbers with macros.

--- cat.c
+++ cat.c
@@ -12,19 +12,21 @@ void do_cat(int fd);
 int main(int argc, char *argv[])
 {
     int fd;
+    int exit_status = EXIT_SUCCESS;
 
     if (argc == 1) {
-        do_cat(0);
+        do_cat(STDIN_FILENO);
         return EXIT_SUCCESS;
     }
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-") == 0) {
-            do_cat(0);
+            do_cat(STDIN_FILENO);
         } else {
             fd = open(argv[i], O_RDONLY);
             if (fd < 0) {
-                perror(argv[i]); 
+                perror(argv[i]);
+                exit_status = EXIT_FAILURE;
                 fprintf(stderr, "Skipping file: %s\n", argv[i]);
                 continue;
             }
@@ -34,19 +36,21 @@ int main(int argc, char *argv[])
         }
     }
 
-    return EXIT_SUCCESS;
+    return exit_status;
 }
 
 void do_cat(int fd)
 {
     char buf[4096];
     ssize_t n;
-    while (0 < (n = read(fd, buf, 4096))) {
-        if(write(1, buf, n) == -1) {
+
+    while (0 < (n = read(fd, buf, sizeof(buf)))) {
+        if (write(STDOUT_FILENO, buf, n) != n) {
             perror("write");
 			exit(EXIT_FAILURE);
         }
     }
+    
     if (n == -1) {
         perror("read");
         exit(EXIT_FAILURE);
